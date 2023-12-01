98dd4c691624a8c4cb5c56ad1f74a1ac0bc3882d
Juan Gallego <gallegoj135@gmail.com>
2023-12-01 18:27:08 -0500

    servidor

--- Server/index.js
+++ Server/index.js
@@ -4,21 +4,23 @@ const { conn } = require('./src/db.js');
 const { apiLoaderProducts } = require("./src/apiLoaderProducts.js");
 const cloudinary = require("cloudinary").v2;
 
-const PORT = 3001;
-//const server = express();
-//server.use(express.json({ upload_max_filesize: "10M" }));
-          
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.use(express.json({ upload_max_filesize: "10M" }));
+
 cloudinary.config({ 
   cloud_name: 'dlhtl7wr4', 
   api_key: '639611433264547', 
   api_secret: 'EqvrtUWaUDEpg4aAChNztTm8SAU' 
 });
 
-
 conn.sync({ force: true }).then(() => {
-server.listen(PORT, async() => {
-  
-  await apiLoaderProducts()
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+  // Cambia la llamada a server.listen por app.listen
+  app.listen(PORT, '0.0.0.0', async() => {
+    await apiLoaderProducts()
+    console.log(`Server listening on port ${PORT}`);
+  });
+}).catch(error => console.error(error));
