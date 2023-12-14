3c0d6c8cf53810bbd410d2c419cd91d77302c0ab
Emiliano Navarro <emilianonavarro1993@gmail.com>
2023-12-14 01:01:28 -0300

    descuento carrito server

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ cloudinary.config({
 });
 
 conn
-  .sync({ force: true })
+  .sync({ force: false})
   .then(() => {
     // Cambia la llamada a server.listen por app.listen
     server.listen(PORT, "0.0.0.0", async () => {
