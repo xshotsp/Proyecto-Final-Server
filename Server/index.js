91df2bc45fb88c85d0ac6f50fcf5348d72ced881
xshotsp <gallegoj135@gmail.com>
2023-12-13 21:23:19 -0500

    cambio

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ cloudinary.config({
 });
 
 conn
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
     // Cambia la llamada a server.listen por app.listen
     server.listen(PORT, "0.0.0.0", async () => {
